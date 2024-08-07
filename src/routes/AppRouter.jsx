b5f136fb29aa011f93578a09f59b7f375aad6ae6
Monserrate Riera <mrieramar@uoc.edu>
2024-08-07 17:05:55 +0200

    feat: Add routes for authentication and journal pages

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,12 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { AuthRoutes } from "../auth/routes/AuthRoutes"
+import { JournalRoutes } from "../journal/routes/JournalRoutes"
 
-
-export const AppRouter = ( ) =>{
+export const AppRouter = () =>{
 
 
 
 
     return (
-        <>
-        </>
+        <Routes>
+            {/* Rutas para el login */}
+            <Route path="/auth/*" element = { <AuthRoutes /> } />
+
+            {/* Rutas para la página en general  */}
+            <Route path="/*" element = { <JournalRoutes /> } />
+        </Routes>
     )
 }
